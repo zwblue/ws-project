3d29216f217ebde19f514bacfafb62556cc249d3
zw <18755489161@163.com>
2018-07-18 10:29:58 +0800

    babel-plugin-transform-decorators-legacy --save-dev 和它的用法

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
+
+import { loadData } from '../../redux/user.redux'
+import { connect } from 'react-redux'
+@connect(
+    state => state.user,
+    { loadData }
+)
+
 class Home extends Component {
+    componentDidMount() {
+        this.props.loadData({ userName: 'zw', msg: '正常运行' });
+    }
     render() {
-        return (<h1>home</h1>)
+        return (
+            <div>
+                <h1>home-{this.props.userName}</h1>
+                <h1>运行情况-{this.props.msg}</h1>
+            </div>
+        )
     }
 }
 export default Home
