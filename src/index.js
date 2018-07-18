d8473f860bf4ccb1112a0dee62c277f67aef7b88
zw <18755489161@163.com>
2018-07-18 10:03:58 +0800

    index.引入redux的东西

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+// redux的引入
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from 'react-redux'
+import reducer from './reducer'
+
+
+// import './config';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 registerServiceWorker();
